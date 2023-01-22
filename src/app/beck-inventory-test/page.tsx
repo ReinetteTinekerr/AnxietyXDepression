ae63264e884081476554357863cad678f6ba7dcf
Reinette <reinettetinekerr@gmail.com>
2023-01-22 21:56:13 +0800

    fix bugs

--- src/app/beck-inventory-test/page.tsx
+++ src/app/beck-inventory-test/page.tsx
@@ -16,8 +16,10 @@ export default function Page() {
     const [registerData, setRegisterData] = useState({ username: "", age: 19, email: "", college: "", course: "" })
     const [bai, setBai] = useState({ total: 0 });
     const [bdi, setBdi] = useState({ total: 0 });
-    const [createdAt, setCreatedAt] = useState();
+    const [createdAt, setCreatedAt] = useState(null);
     const [comment, setComment] = useState("");
+    const [showPopup, setShowPopup] = useState(false);
+    const [submitted, setSubmitted] = useState(false);
 
     useEffect(() => {
         const id = localStorage.getItem("id");
@@ -37,6 +39,10 @@ export default function Page() {
             })
         }
     }, []);
+
+    const onClickPopup = () => {
+        setShowPopup(false);
+    }
 
     const onClickNext = () => {
         if (renderIndex === 0) {
@@ -59,6 +65,8 @@ export default function Page() {
                     console.log(res.id);
                     localStorage.setItem("id", res.id);
                 })
+                setShowPopup(true);
+                setSubmitted(true);
             } catch (error) {
 
                 console.log(error);
@@ -81,6 +89,25 @@ export default function Page() {
                     <div className="w-full text-center">
                         <h2 className="text-3xl md:text-5xl font-bold leading-snug text-white mb-10 wow fadeInUp" data-wow-delay="1s">Beck Anxiety
                             <br className="block" />and<br className="block" />Beck Depression</h2>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        {/* Modal */}
+        <section>
+            <div id="popup-modal" className={`${showPopup ? "" : "hidden"} fixed top-0 left-1/2 right-1/2 z-50 p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full`}>
+                <div className="relative w-full h-full max-w-md md:h-auto">
+                    <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                        <button type="button" className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-hide="popup-modal">
+                            <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                            <span className="sr-only">Close modal</span>
+                        </button>
+                        <div className="p-6 text-center">
+                            {/* <svg aria-hidden="true" className="mx-auto mb-4 text-gray-400 w-14 h-14 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg> */}
+                            <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Thank You for Responding!</h3>
+                            <button onClick={onClickPopup} data-modal-hide="popup-modal" type="button" className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Ok</button>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -117,7 +144,7 @@ export default function Page() {
                         <button onClick={onClickPrevious} disabled={renderIndex === 0} className="mx-4 text-white disabled:bg-gray-400 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">
                             Prev
                         </button>
-                        <button ref={buttonRefNext} onClick={onClickNext} disabled={renderIndex == 3 && (Object.values(bdi).length < 21 || Object.values(bai).length < 21)} className="text-white disabled:bg-gray-400 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">
+                        <button ref={buttonRefNext} onClick={onClickNext} disabled={(renderIndex == 3 && (Object.values(bdi).length < 21 || Object.values(bai).length < 21) || submitted)} className="text-white disabled:bg-gray-400 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">
                             {renderIndex === 3 ? "Submit" : "Next"}
                         </button>
                     </div>
